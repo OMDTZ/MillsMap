628908b7b76afb785e9204c7ee662009adf5b99e
edgar <mloweedgar0@gmail.com>
2023-03-03 14:36:45 +0300

    feat(sidebar): made sidebar responsibe in for computer screens of different sizes

--- app/static/js/toggleNavbar.js
+++ app/static/js/toggleNavbar.js
@@ -3,13 +3,15 @@ const toggleNavbar = () => {
     // toggle the left-side navbar
     $(document).ready(function () {
         $('.sidebar__button--close').on('click', function () {
-            $('.sidebar').css('width', '4%');
+            $('.sidebar').addClass('sidebar_width--closed');
+            $('.sidebar').removeClass('sidebar_width--opened');
             $('.sidebar--opened').addClass('hide');
             $('.sidebar--closed').removeClass('hide');
         });
 
         $('.sidebar__button--open').on('click', function () {
-            $('.sidebar').css('width', '22%');
+            $('.sidebar').addClass('sidebar_width--opened');
+            $('.sidebar').removeClass('sidebar_width--closed');
             $('.sidebar--opened').removeClass('hide');
             $('.sidebar--closed').addClass('hide'); 
         });
