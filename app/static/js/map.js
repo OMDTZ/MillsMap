ce8fc553efa1531850427923ef0c3438ca3a6ebc
edgar <mloweedgar0@gmail.com>
2023-03-04 15:04:59 +0300

    refactor(map legend): load the html code for legend from legend.html file

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -69,18 +69,26 @@ const createMapIcons = (map) => {
     // Legend
     var legend = L.control({ position: "bottomleft" });
 
+    // legend.onAdd = function (map) {
+    //     var div = L.DomUtil.create("div", "legend");
+    //     div.innerHTML += "<h4>Number of Schools</h4>";
+    //     div.innerHTML += '<i style="background: #7cc247"></i><span>Less than 10</span><br>';
+    //     div.innerHTML += '<i style="background: #f7eb65"></i><span>10 to 100</span><br>';
+    //     div.innerHTML += '<i style="background: #ff7438"></i><span>More than 100</span><br>';
+    //     div.innerHTML += '<i style="background: style="background-image: url(https://d30y9cdsu7xlg0.cloudfront.net/png/194515-200.png);background-repeat: no-repeat;"></i><span></span><br>';
+
+
+
+    //     return div;
+    // };
+
     legend.onAdd = function (map) {
-        var div = L.DomUtil.create("div", "legend");
-        div.innerHTML += "<h4>Number of Schools</h4>";
-        div.innerHTML += '<i style="background: #7cc247"></i><span>Less than 10</span><br>';
-        div.innerHTML += '<i style="background: #f7eb65"></i><span>10 to 100</span><br>';
-        div.innerHTML += '<i style="background: #ff7438"></i><span>More than 100</span><br>';
-        div.innerHTML += '<i style="background: style="background-image: url(https://d30y9cdsu7xlg0.cloudfront.net/png/194515-200.png);background-repeat: no-repeat;"></i><span></span><br>';
-
-
-
+        var div = L.DomUtil.create('div', 'legend');
+        var legendContent = document.getElementById('legend-content').innerHTML;
+        div.innerHTML = legendContent;
         return div;
     };
+
 
     legend.addTo(map);
 }
