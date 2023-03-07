76c6fc278544ba1bae4cec4906b784de6a638a6a
edgar <mloweedgar0@gmail.com>
2023-03-07 12:17:19 +0300

    feat(navigation bar): activate/deactivate nav items

--- app/static/js/toggleNavbar.js
+++ app/static/js/toggleNavbar.js
@@ -2,42 +2,36 @@ const toggleNavbar = () => {
     console.log("toggleNavbar.js loaded");
     // toggle the left-side navbar
     $(document).ready(function () {
+
+        // toggle the left-side navbar
         $('.sidebar__button--close').on('click', function () {
             $('.sidebar').addClass('sidebar_width--closed');
             $('.sidebar').removeClass('sidebar_width--opened');
             $('.sidebar--opened').addClass('hide');
             $('.sidebar--closed').removeClass('hide');
         });
-
         $('.sidebar__button--open').on('click', function () {
             $('.sidebar').addClass('sidebar_width--opened');
             $('.sidebar').removeClass('sidebar_width--closed');
             $('.sidebar--opened').removeClass('hide');
             $('.sidebar--closed').addClass('hide'); 
         });
+
+        // Navitem active state
+        $('.top_navbar__button').on('click', function () {
+            console.log(" nav clicked");
+            $('.top_navbar__button').removeClass('top_navbar__button--active');
+            $('.page').addClass('hide');
+            $(this).addClass('top_navbar__button--active');
+            let page = $(this).attr('data-page');
+            $(`#${page}`).removeClass('hide');
+            if(page === 'home') {
+                $('#infographics').removeClass('hide');
+            }
+            else {
+                $('#infographics').addClass('hide');
+            }
+        });
       
-    });
-
-    $(document).ready(function () {
-        $('#filterToggle').on('click', function () {
-            $('.side-column').addClass('hide')
-            $('.nav-link').removeClass('active')
-            $('#mapbar').addClass('col-6')
-            $('#mapbar').removeClass('col-8')
-            $('#selects').toggleClass('hide');
-            $('#infographics').toggleClass('hide');
-            $('#filterToggle').toggleClass('active');
-        });
-    });
-
-    $(document).ready(function () {
-        $('#howtoToggle').on('click', function () {
-            $('.side-column').addClass('hide')
-            $('.nav-link').removeClass('active')
-            $('#mapbar').addClass('col-8')
-            $('#mapbar').removeClass('col-6')
-            $('#howto').toggleClass('hide');
-            $('#howtoToggle').toggleClass('active');
-        });
     });
 }
