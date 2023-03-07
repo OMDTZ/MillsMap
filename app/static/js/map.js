cbcc385f26027f853cbfdba16213b55422ded788
edgar <mloweedgar0@gmail.com>
2023-03-07 14:58:44 +0300

    feat(maps): add scroll indicator to let user know they can scroll down

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -87,7 +87,7 @@ const createMapIcons = (map) => {
         var div = L.DomUtil.create('div', 'toggle_filters');
         var button = `
                             <button class="toggle_filters__button">
-                                    <svg height="20" fill="none" stroke="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <svg height="30" fill="none" stroke="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"></path>
                                     </svg>
                             </button>
@@ -97,6 +97,20 @@ const createMapIcons = (map) => {
         return div;
     };
     toggleFiltersButton.addTo(map);
+
+    // Add scroll indicator
+    var scroll = L.control({ position: "bottomright" });
+    scroll.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'scroll_indicator');
+        var button = `
+        <svg fill="white" height="70" viewBox="0 0 24 24" style="transform: rotate(270deg);">
+        <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>
+      </svg>
+        `;
+        div.innerHTML = button;
+        return div;
+    };
+    scroll.addTo(map);
 
     // open/close filters when clicking on the button
     $('.toggle_filters__button').click(function () {
