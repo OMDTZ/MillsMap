47ffe3a9e058d271fcd8ce4cba01522c949f210f
edgar <mloweedgar0@gmail.com>
2023-03-07 10:34:26 +0300

    feat(map filters): added styles for the buttons

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -84,7 +84,7 @@ const createMapIcons = (map) => {
         var div = L.DomUtil.create('div', 'toggle_filters');
         var button = `
                             <button class="toggle_filters__button">
-                                    <svg height="16" fill="none" stroke="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <svg height="20" fill="none" stroke="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"></path>
                                     </svg>
                             </button>
@@ -98,6 +98,14 @@ const createMapIcons = (map) => {
     // open/close filters when clicking on the button
     $('.toggle_filters__button').click(function () {
         $('.map_filters').toggleClass('hide');
+        // modify the button border and background color when the filters are open
+        if ($('.map_filters').hasClass('hide')) {
+            $('.toggle_filters__button').css('border-color', '#30619c');
+            $('.toggle_filters__button').css('background', '#30619c');
+        } else {
+            $('.toggle_filters__button').css('border-color', '#ffc107');
+            $('.toggle_filters__button').css('background', '#ffc107');
+        }
     });
 }
 
